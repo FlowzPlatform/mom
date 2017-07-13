5b5ee7b14434fe45b1960ecbcd5c5583989aaf67
software <kkachhiya@officebrain.com>
2017-07-13 12:41:16 +0530

    changes

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -2,7 +2,7 @@
 import feathers from 'feathers/client'
 import socketio from 'feathers-socketio/client'
 import io from 'socket.io-client'
-const socket = io('http://localhost:3030')
+// const socket = io('http://localhost:3030')
 export const app = feathers().configure(socketio(socket));
 
 export const tasksService = app.service('tasks')
