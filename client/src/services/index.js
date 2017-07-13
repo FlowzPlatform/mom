4e0f18f3f7e849975b952d5a396ff2dc3317c98f
Urvashi Hirani <uhirani@officebrain.com>
2017-07-13 20:08:21 +0530

    changed in socket file

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -2,7 +2,8 @@
 import feathers from 'feathers/client'
 import socketio from 'feathers-socketio/client'
 import io from 'socket.io-client'
-// const socket = io('http://localhost:3030')
+
+const socket = io(process.env.SOCKET_IO);
 export const app = feathers().configure(socketio(socket));
 
 export const tasksService = app.service('tasks')
