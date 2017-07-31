125f1ea95f8a680c98c56a34ffaf353bf98ebbd7
software <kkachhiya@officebrain.com>
2017-07-31 19:21:10 +0530

    Insert, update and delete query

--- server/src/services/task_type/hooks/index.js
+++ server/src/services/task_type/hooks/index.js
@@ -1,7 +1,6 @@
 'use strict';
 exports.before = {
   all: [],
-<<<<<<< a1778d5ac936ad9a9c546e36fabb5dc1988f4913
   find(hook){
     const query = this.createQuery(hook.params.query);
     const r = this.options.r;
@@ -9,9 +8,6 @@ exports.before = {
     hook.params.rethinkdb =query.orderBy('createdAt')
   },
   // find: [],
-=======
-  find: [],
->>>>>>> task type get from db
   get: [],
   create: [],
   update: [],
