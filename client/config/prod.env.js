32f5b0e2c7b2af08f00b9eaa72bdc060cf91364b
Urvashi Hirani <uhirani@officebrain.com>
2017-08-31 16:05:21 +0530

    changed dev.env file

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
-  SOCKET_IO: '"http://162.242.223.167:3030"',
-  USER_AUTH: '"http://162.242.223.167:3001"',
-  USER_DETAIL: '"http://162.242.223.167:3002"',
-  COPY_URL_PATH: '"http://162.242.223.167:3000"'
+  SOCKET_IO: '"http://localhost:3030"',
+  USER_AUTH: '"http://localhost:3001"',
+  USER_DETAIL: '"http://localhost:3002"',
+  COPY_URL_PATH: '"http://localhost:3000"'
 }
