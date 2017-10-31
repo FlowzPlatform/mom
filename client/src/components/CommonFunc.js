00c5fcf10b185b50811100d6f521cad687e7a0dc
Hemant Patel <hpatel@officebrain.com>
2017-10-31 16:15:59 +0530

    attachment preview.

--- client/src/components/CommonFunc.js
+++ client/src/components/CommonFunc.js
@@ -193,7 +193,7 @@ export default {
   {
     services.taskHistoryLogs.create({created_by:createdBy,text:text,task_id:taskId,log_action:logAction,created_on:new Date()}).then(response=> {
       console.log("Reponse task update:--->",response)
-      context.state.taskHistoryLog.push(response)
+      context.state.taskHistoryLog.unshift(response)
     })
   },
   /**
