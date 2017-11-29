2fc94e8a3ee7b5658d1890028a5e354a176feb98
Urvashi Hirani <uhirani@officebrain.com>
2017-11-29 16:45:17 +0530

    changed related to generate docker

--- client/config/index.js
+++ client/config/index.js
@@ -19,7 +19,7 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: true //process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report //true
   },
   dev: {
     env: require('./dev.env'),
@@ -34,6 +34,6 @@ module.exports = {
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
     cssSourceMap: false,
-    bundleAnalyzerReport: true   
+    // bundleAnalyzerReport: true   
   }
 }
--- client/src/components/Attachments.vue
+++ client/src/components/Attachments.vue
@@ -107,7 +107,7 @@
     /* eslint-disable*/
     import Vue from "vue";
     import { mapGetters } from "vuex";
-    import "vue-ionicons/ionicons/css/ionicons.css";
+    // import "vue-ionicons/ionicons/css/ionicons.css";
     import iView from "iview";
     import "iview/dist/styles/iview.css";
     import Avatar from "vue-avatar/dist/Avatar";
