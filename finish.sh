e6d1b9792c300b1d0b2a5f144214ad1b2d129b1d
Urvashi Hirani <uhirani@officebrain.com>
2017-12-19 11:26:41 +0530

    changed in travis.yml, finish, upgrade and docker of client

--- finish.sh
+++ finish.sh
@@ -1,3 +1,7 @@
 curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
 'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s120?action=finishupgrade'
+
+curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+-X POST \
+'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s302?action=finishupgrade'
